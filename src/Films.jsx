bfe137d39e50895425a51104e658086ee4e0bdd6
Eric Cote <eric@coteexpert>
2023-03-06 20:59:49 -0500

    appel async à un service rest

--- src/Films.jsx
+++ src/Films.jsx
@@ -1,4 +1,25 @@
+import { useEffect, useState } from 'react';
+// on pourrait utiliser:
+// XHR (XmlHttpRequest)
+// jQuery (ajax)
+// axios
+// fetch (html5)
+
 export default function Films() {
+  const [films, setFilms] = useState([]);
+
+  async function chargerFilms() {
+    const response = await fetch(
+      'https://mcuapi.herokuapp.com/api/v1/movies?page=1&limit=50'
+    );
+    const data = await response.json();
+    setFilms(data);
+  }
+
+  useEffect(() => {
+    chargerFilms();
+  }, []);
+
   return (
     <>
       <h1>Films Marvel</h1>
