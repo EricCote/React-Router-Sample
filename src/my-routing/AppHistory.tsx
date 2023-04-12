57e25b144f9a26b8bc60be7ce4c5ebd470ededfb
Eric Cote <eric@coteexpert.com>
2023-04-12 02:23:35 -0400

    app History changes

--- src/my-routing/AppHistory.tsx
+++ src/my-routing/AppHistory.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import Menu from './Menu';
 import Route from './Route';
@@ -13,20 +13,22 @@ function App() {
     });
   }, []); //bind this event on the first render.
 
-  //Modify existing links, instead of using a <Link to="/dest" /> component
   useEffect(modifyLinksToAddClickHandler, []);
 
+  //Modify existing links, instead of using a <Link to="/dest" /> component
   function modifyLinksToAddClickHandler() {
     // Select all huperlinks
     for (const link of document.querySelectorAll(
       ':link'
     ) as NodeListOf<HTMLLinkElement>) {
+      //bind a click event handler
       link.addEventListener('click', (evt) => {
-        clickHandler(evt, link); //modify the click event with a special click handler
+        clickHandler(evt, link);
       });
     }
   }
 
+  //click handler that uses the history API instead of navigating
   function clickHandler(evt: Event, link: HTMLLinkElement) {
     //create a url object from the link text
     const url = new URL(link.href);
@@ -37,7 +39,8 @@ function App() {
       url.pathname !== window.location.pathname
     ) {
       evt.preventDefault(); //cancel the navigation
-      window.history.pushState(url.pathname, '', url.pathname); //Add destination in history
+      //Add destination in history API
+      window.history.pushState(url.pathname, '', url.pathname);
       setCurrentPage(url.pathname); //set State to re-render the app.
     }
   }
